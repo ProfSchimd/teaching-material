1956aa74706ffc5583e184026478be69c8624031
ProfSchimd <michele.schimd@itiszuccante.edu.it>
2023-03-31 09:45:49 +0200

    INF: prepared code for JSON and DOM lab.
    The code added is a simple parsing of a JSON file which is used
    to modify the DOM using basic Javascript primitives. The JSON is
    retrieved using fetch.

--- inf/javascript/json-dom/script.js
+++ inf/javascript/json-dom/script.js
@@ -1,3 +1,61 @@
+function domAccessExamples() {
+    // element (not necessarily a div) with id content
+    const contentElement = document.querySelector('#content');
+    console.log(contentElement); 
+    // >> <div id="content">
+
+    // get the first h2 having the class subtitle
+    const subtitleH2 = document.querySelector('h2.subtitle');
+    console.log(subtitleH2);
+    // >> <h2 class="subtitle">
+
+    // get all elements of type div
+    const allDiv = document.querySelectorAll('div');
+    console.log(allDiv);
+    // >> NodeList(3) [ div#header, ... ]
+}
+
+function createItemDiv() {
+    return document.createElement('div');
+}
+
+function available(stock) {
+    const element = document.createElement('p');
+    if (stock < 5) {
+        element.append('Solo ');
+        element.classList.add('warning');
+    }
+    element.append(stock + ' disponibili');
+    return element;
+}
+
+function showProducts(json) {
+    console.log(json.products);
+    contentDiv = document.querySelector('#content');
+    for (let item of json.products)
+    {
+        const newDiv = document.createElement('div');
+        newDiv.classList.add('with-border');
+        const h4Element = document.createElement('h4');
+        h4Element.append(item.name);
+        const descriptionParagraph = document.createElement('p');
+        descriptionParagraph.append(item.description);
+        const priceParagraph = document.createElement('p');
+        priceParagraph.append(item.price + ' €');
+        const stockElement = available(item.stock);
+        newDiv.append(h4Element, descriptionParagraph, priceParagraph, stockElement);
+        contentDiv.append(newDiv);
+    }
+}
+
 function main() {
-    console.log('Hello World!');
+    domAccessExamples();
+    console.log(createItemDiv());
+    
+    const jsonUrl = 'https://raw.githubusercontent.com/ProfSchimd/teaching-material/main/inf/javascript/json-dom/data.json';
+    fetch(jsonUrl)
+        .then(res => res.json())
+        .then(json => showProducts(json))
+        .catch(err => console.log(err));
+    
 }
