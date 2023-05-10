acf11789622e5f418a3da52fca9588e420489f18
ProfSchimd <michele.schimd@itiszuccante.edu.it>
2023-05-10 08:18:21 +0200

    INF: Added stub implementation of Map.
    The class Map implements (generics) class Map<K,V> but doesn't
    give any method implementation yet.

---
+++ inf/datastructure/maps/java/Map.java
@@ -0,0 +1,45 @@
+public class Map<K,V> implements IMapGenerics<K,V> {
+
+    @Override
+    public void add(K key, V value) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'add'");
+    }
+
+    @Override
+    public V remove(K key) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'remove'");
+    }
+
+    @Override
+    public V get(K key) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'get'");
+    }
+
+    @Override
+    public K[] keys() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'keys'");
+    }
+
+    @Override
+    public V[] values() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'values'");
+    }
+
+    @Override
+    public int size() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'size'");
+    }
+
+    @Override
+    public boolean isEmpty() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isEmpty'");
+    }
+    
+}
