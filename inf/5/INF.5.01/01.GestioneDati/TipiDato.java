a1e1c0d4177086b60bc745c8701f3276ccbf2dde
ProfSchimd <michele.schimd@itiszuccante.edu.it>
2023-06-29 09:14:08 +0200

    [INF]: Added INF.5.01 and subdir

---
+++ inf/5/INF.5.01/01.GestioneDati/TipiDato.java
@@ -0,0 +1,14 @@
+public class TipiDato {
+    public static void main(String[] args) {
+        int a = 123;
+        boolean b = false;
+        char c = 'Z';
+        double d = 3.1415;
+        String e = "Hello!";
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+    }
+}
