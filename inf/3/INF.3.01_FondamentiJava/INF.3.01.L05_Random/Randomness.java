2d665404b88c6d7b7ed6af54f810138e0f5d851e
ProfSchimd <91139486+ProfSchimd@users.noreply.github.com>
2024-10-08 15:24:03 +0200

    [INF]: Updated Randomness.java file

--- inf/3/INF.3.01_FondamentiJava/INF.3.01.L05_Random/Randomness.java
+++ inf/3/INF.3.01_FondamentiJava/INF.3.01.L05_Random/Randomness.java
@@ -14,11 +14,18 @@ public class Randomness {
         System.out.println("nextInt(a)    " + randomNatural);
         System.out.println("nextDouble()  " + randomDouble);
 
+        // Generiamo una sequenza casuale di caratteri esadecimale
+        String s = "0123456789ABCDEF";
+        for (int i = 0; i < 50; i++) {
+            System.out.print(s.charAt(rand.nextInt(s.length()))); // !!!! length() è un METODO
+        }
+        System.out.println();
+
         // Il meccanismo del 'seeding' (piantare un seme) permette di generare
         // sempre gli stessi numeri casuali (casuali???)
 
         for (int i = 0; i < 10; i++) {
-            rand.setSeed(42);
+            rand.setSeed(-111);
             System.out.println(rand.nextInt());
             System.out.println(rand.nextInt(1000));
             System.out.println(rand.nextDouble());
