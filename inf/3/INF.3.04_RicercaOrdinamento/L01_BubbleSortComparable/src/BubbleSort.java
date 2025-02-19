036239d3c09a1f22b268c4e1a69db18b9933b639
ProfSchimd <91139486+ProfSchimd@users.noreply.github.com>
2025-02-19 20:15:27 +0100

    [INF] - Added improvedBubbleSort Java method

--- inf/3/INF.3.04_RicercaOrdinamento/L01_BubbleSortComparable/src/BubbleSort.java
+++ inf/3/INF.3.04_RicercaOrdinamento/L01_BubbleSortComparable/src/BubbleSort.java
@@ -12,19 +12,6 @@ public class BubbleSort {
         }
     }
 
-    // public static void bubbleSort(Player[] a) {
-    //     int n = a.length;
-    //     for (int i = 0; i < n - 1; i++) {
-    //         for (int j = 0; j < n - i - 1; j++) {
-    //             if (a[j].getScore() > a[j + 1].getScore()) {
-    //                 Player temp = a[j];
-    //                 a[j] = a[j + 1];
-    //                 a[j + 1] = temp;
-    //             }
-    //         }
-    //     }
-    // } 
-
     public static void bubbleSort(Comparable[] a) {
         int n = a.length;
         for (int i = 0; i < n - 1; i++) {
@@ -38,13 +25,31 @@ public class BubbleSort {
         }
     } 
 
+    public static void improvedBubbleSort(double[] a) {
+        int n = a.length;
+        for (int i = 0; i < n - 1; i++) {
+            boolean inversions = false;
+            for (int j = 0; j < n - i - 1; j++) {
+                if (a[j] > a[j + 1]) {
+                    inversions = true;
+                    double temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
+                }
+            }
+            if (!inversions) {
+                break;
+            }
+        }
+    }
+
     public static void main(String[] args) {
         double[] a = {5.4, 2.1, 9.7, 1.5, 4.3, 8.2};
         
         System.out.println("Original array:");
         Util.printDoubleArray(a);
         
-        bubbleSort(a);
+        improvedBubbleSort(a);
 
         System.out.println("Sorted array:");
         Util.printDoubleArray(a);
